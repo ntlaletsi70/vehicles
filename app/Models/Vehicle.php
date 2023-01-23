6e243ecff11475f87a66f28bcbd57a4586c41ef2
Neo Tlaletsi <ntlaletsi86@gmail.com>
2023-01-23 19:24:09 +0200

    Update Vehicle.php

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
+    //to generate code comments for documentation
     use HasFactory;
     protected $fillable = [
         'make', 'model','year_of_manufacture', 'price'
